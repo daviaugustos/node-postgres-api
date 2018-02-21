3ef3647dc7fa513fd5e13607335adb8a8f4b2876
Davi Santos <davisantos@gsgroup.com.br>
2018-02-21 08:53:46 -0300

    Endpoint POST users com delay

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const { Client  } = require('pg');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -8,40 +7,28 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'tradeappdb',
-    password: '123qweasd',
-    port: 5432,
-});
-client.connect();
-  
+ 
 app.get('/users', async (req, res) => {
     const data = {
         login: 'Usuário teste',
         password: 'senhaTeste'
     };
 
-    await setInterval(()=>{
+    await setTimeout(()=>{
         res.status(200).json(data);
     },  5000);
 });
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const { login, password } = req.body;
-    const query = {
-        text: 'INSERT INTO users(login, password) VALUES($1, $2)',
-        values: [login, password],
+
+    const data = {
+        sucess: true,
+        login: login,
+        password: password
     };
 
-    client.query(query)
-        .then((resultSet) => {
-            const data = resultSet;
-            res.status(200).json(data);
-        })
-        .catch((error) => {
-            res.status(500).json(error);
-        });
+    await setTimeout(()=>{
+        res.status(200).json(data);
+    },  5000);
 });
