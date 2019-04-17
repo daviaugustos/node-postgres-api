b4b5dfce77b38df754b45d5cd3a8a0e096570ae1
Davi Santos <davi.santos@ciatecnicasolutions.com.br>
2019-04-17 14:09:57 -0300

    New error credentials endpoint to test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(cors());
@@ -8,29 +8,41 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('Example app listening on port' + port));
- 
-app.get('/users', async (req, res) => {
-    const data = {
-        login: 'Usuário teste',
-        password: 'senhaTeste'
-    };
-    console.log("/users - GET", data);
-    await setTimeout(()=>{
-        res.status(200).json(data);
-    },  5000);
+app.listen(port, () => console.log("Example app listening on port" + port));
+
+app.get("/users", async (req, res) => {
+  const data = {
+    login: "Usuário teste",
+    password: "senhaTeste"
+  };
+  console.log("/users - GET", data);
+  await setTimeout(() => {
+    res.status(200).json(data);
+  }, 5000);
 });
 
-app.post('/users', async (req, res) => {
-    const { login, password } = req.body;
+app.post("/auth", async (req, res) => {
+  const { login, password } = req.body;
 
-    const data = {
-        sucess: true,
-        login: login,
-        password: password
-    };
-    console.log("/users - POST", data);
-    await setTimeout(()=>{
-        res.status(200).json(data);
-    },  5000);
+  const data = {
+    invalid_action_message: "",
+    login: login,
+    password: password
+  };
+  console.log("/users - POST", data);
+  await setTimeout(() => {
+    res.status(200).json(data);
+  }, 5000);
+});
+
+app.post("/auth/error", async (req, res) => {
+  const { login, password } = req.body;
+
+  const data = {
+    invalid_action_message: "invalid_credentials"
+  };
+  console.log("/users - POST", data);
+  await setTimeout(() => {
+    res.status(200).json(data);
+  }, 5000);
 });
