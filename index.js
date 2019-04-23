1a6fd79c37b5bf8ae0ba3300c34d629259091452
Davi Santos <davi.santos@ciatecnicasolutions.com.br>
2019-04-23 17:28:45 -0300

    Adding form types with token validation

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  if (req.path == "/auth/error" || req.path == "/users" || req.path == "/auth")
+    return next();
+
+  if (!req.get("Authorization")) {
+    return res
+      .status(403)
+      .json({ invalid_action_message: "no_credentials_sent" });
+  }
+  next();
+});
 
 const port = process.env.PORT || 8080;
 
@@ -42,17 +53,37 @@ app.post("/auth", async (req, res) => {
     ],
     token: "Basic Q2FkQ2xpZW50ZTpMVFpleGxzcnYxMjA5"
   };
-  console.log("/users - POST", data);
+  console.log("/auth - POST", data);
   await setTimeout(() => {
     res.status(200).json(data);
   }, 5000);
 });
 
+//endpoint criado apenas para demonstrar como seria uma resposta com credenciais invalidas
 app.post("/auth/error", async (req, res) => {
   const { login, password } = req.body;
 
   const data = {
     invalid_action_message: "invalid_credentials"
+  };
+  console.log("/auth/error - POST", data);
+  await setTimeout(() => {
+    res.status(200).json(data);
+  }, 5000);
+});
+
+app.get("/formularios/tipos", async (req, res) => {
+  const data = {
+    form_types: [
+      {
+        id: 1,
+        name: "Housekeeping"
+      },
+      {
+        id: 2,
+        name: "Inspeção"
+      }
+    ]
   };
   console.log("/users - POST", data);
   await setTimeout(() => {
